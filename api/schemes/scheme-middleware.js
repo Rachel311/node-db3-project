71c63575d31d87f0ddfc0b3f0c8b7a1e258555f0
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-21 21:31:38 -0600

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,26 +1,37 @@
-/*
-  If `scheme_id` does not exist in the database:
+const Scheme = require('./scheme-model');
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const schemeId = await Scheme.findById(req.params.scheme_id);
+    if (schemeId) {
+      req.scheme_id = schemeId;
+      next();
+    } 
+  } catch (err) {
+    next ({
+      message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      status: 404
+    });
   }
-*/
-const checkSchemeId = (req, res, next) => {
+};
 
-}
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
+const validateScheme = async (req, res, next) => {
+  try {
+    const schemeName = req.body;
+    if (!schemeName || typeof schemeName != 'string') {
+      next({
+        message: 'invalid scheme_name',
+        status: 400
+      });
+    } else {
+      next ();
+    }
+  } catch (err) {
+    next(err);
   }
-*/
-const validateScheme = (req, res, next) => {
-
-}
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,9 +42,27 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = async (req, res, next) => {
+  try {
+    const { instructions, step_number } = req.body;
+    if (!instructions || typeof instructions !== 'string') {
+      next({
+        message: 'invalid step',
+        status: 400
+      });
+    }
+    if (typeof step_number !== 'number' || step_number < 1) {
+      next({
+        message: 'invalid step',
+        status: 400
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   checkSchemeId,
